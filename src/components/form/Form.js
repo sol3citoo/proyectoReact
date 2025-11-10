fc412c0b28d76d24db366edddee3a6aba16acedb
sol3citoo <devarodriguezgomez@gmail.com>
2025-11-10 13:53:12 +0100

    Agregar cambios para UT04_ACT1

---
+++ src/components/form/Form.js
@@ -0,0 +1,74 @@
+import './Form.css'
+import React, {useState} from "react";
+
+
+class Form extends React.Component {
+envioFormulario = (event)=>{
+    event.preventDefault();
+    const form = event.target;
+    this.props.agregarIncidencia(
+        form.titulo.value,
+        form.usuario.value,
+        form.descripcion.value,
+        form.categoria.value,
+        form.nivel.value,
+        form.ubicacion.value
+    )
+}
+
+
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.envioFormulario}>
+                    
+                    {/* Título */}
+                    <div className="elemento-form">
+                        <label>Título</label>
+                        <input type="text" name="titulo" placeholder="Problema general" required/><br></br>
+                    </div>
+                    {/* Usuario */}
+                    <div className="elemento-form">
+                        <label>Usuario</label>
+                        <input type="text" name="usuario" placeholder="Introduce el id del usuario" required/><br></br>
+                    </div>
+                    {/* Descripción */}
+                    <div className="elemento-form">
+                        <label>Descripción</label>
+                        <textarea name="descripcion" required/><br></br>
+                    </div>
+                    {/* Categoría */}
+                    <div className="elemento-form">
+                        <label>Categoría</label>
+                            <select name="categoria" required>
+                                <option value="">Seleccionar...</option>
+                                <option>Hardware</option>
+                                <option>Software</option>
+                                <option>Red y conectividad</option>
+                                <option>Usuarios y accesos</option>
+                                <option>Infraestructura</option>
+                            </select>
+                    </div>
+                    {/* Urgencia */}
+                    <div className="elemento-form">
+                        <label>Nivel de urgencia</label>
+                            <select name="nivel" required>
+                                <option value="">Seleccionar...</option>
+                                <option>Alta</option>
+                                <option>Media</option>
+                                <option>Baja</option>
+                            </select>
+                    </div>
+                    {/* Ubicación */}
+                    <div className="elemento-form">
+                        <label>Ubicación</label>
+                        <input type="text" name="ubicacion" placeholder="Ubicación de la incidencia" required/>
+                    </div>
+                    {/* Botón */}
+                    <button type="submit" className="elemento-form-button"> Registrar </button>
+                </form>
+            </div>
+        )
+    }
+}
+export default Form;
--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,20 +1,95 @@
+import React from 'react';
 import './App.css';
-import MiLista from '../milista';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import MiLista from '../MiLista';
+import Form from '../form/Form';
 
-function App() {
+
+class App extends React.Component {
+  state={incidencias:[ 
+        {
+            id_incidencia: 1,
+            id_usuario: 'e768590345h',
+            titulo: "Proyector averiado en aula 2",
+            descripcion: "El proyector no enciende y la lámpara parece dañada.",
+            categoria: "Hardware",
+            nivel_urgencia: "Alta",
+            fecha_registro: "2025-10-03",
+            estado: "Abierta",
+            ubicacion: "A301"
+        },
+        {
+            id_incidencia: 2,
+            id_usuario: 'e235439802s',
+            titulo: "Ordenador de secretaría no enciende",
+            descripcion: "El equipo no responde al precionar el botón encendido.",
+            categoria: "Hardware",
+            nivel_urgencia: "Media",
+            fecha_registro: "2025-10-02",
+            estado: "En proceso",
+            ubicacion: "B205"
+        },
+        {
+            id_incidencia: 3,
+            id_usuario: 'e765849381b',
+            titulo: "Impresora sin conexión",
+            descripcion: "La impresora de la sala de profesores no aparece en red.",
+            categoria: "Red/Impresión",
+            nivel_urgencia: "Alta",
+            fecha_registro: "2025-10-01",
+            estado: "Resuelta",
+            ubicacion: "Sala de profesores"
+        }
+    ]
+    }
+
+
+    
+    agregarIncidencia =(nuevo_usuario, nuevo_titulo, nuevo_descripcion,
+                        nuevo_categoria, nuevo_nivel_urgencia, nuevo_ubicacion)=>{
+            const fecha = new Date(); 
+            const year = fecha.getFullYear(); 
+            const month = String(fecha.getMonth() + 1).padStart(2, '0'); // meses 0-11 
+            const day = String(fecha.getDate()).padStart(2, '0'); 
+            const fechaFormateada = year+"-"+month+"-"+day; 
+            const nueva_incidencia = {
+            id_incidencia: this.state.incidencias.length+1,
+            id_usuario: nuevo_usuario,
+            titulo: nuevo_titulo,
+            descripcion: nuevo_descripcion,
+            categoria: nuevo_categoria,
+            nivel_urgencia: nuevo_nivel_urgencia,
+            fecha_registro: fechaFormateada,
+            estado: "Abierta",
+            ubicacion: nuevo_ubicacion
+        }
+        this.setState({
+            incidencias: [...this.state.incidencias, nueva_incidencia]
+        });
+        console.log("Datos recibidos: ", nueva_incidencia);
+     }
+
+
+  render(){
   return (
     <>
       <Header/>
       <h2>Mi aplicación</h2>
-      <div id="parrafo">
+      <div className='contenedor-incidencias'>
+        <main>
         <p>Bienvenido a mi aplicación, esto fue creado con JavaScript en React</p>
+        <MiLista incidencias={this.state.incidencias}/>
+        </main>
+        <aside>
+        <Form agregarIncidencia={this.agregarIncidencia}/>
+        </aside>
       </div>
-      <MiLista/>
+      <br/>
       <Footer/>
     </>
   );
 }
-
+}
 export default App;
+
